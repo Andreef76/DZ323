e61426ebde84486c13f1b749db1f41183381afb2
Andreef76 <121039918+Andreef76@users.noreply.github.com>
2023-02-20 20:41:01 +0500

    Add commit

---
+++ bin/main/Main.kt
@@ -0,0 +1,25 @@
+import kotlin.math.roundToInt
+
+fun main() {
+    val itemPrice = 100
+    val itemCount = 121
+    val discount = 100
+    val discountStart = 1_000
+    val regularCustomer: Boolean = true
+    val totalPrice = itemPrice * itemCount
+    var result = 0
+
+    if (totalPrice > discountStart && totalPrice < 10_001) {
+        result = totalPrice - discount
+    } else if (totalPrice > 10_000) {
+        result = (totalPrice * 0.95).toInt()
+    } else if (totalPrice < 1001) {
+        result = totalPrice
+    }
+
+    if (regularCustomer == true) {
+        result = (result * 0.99).toInt()
+    }
+
+    println("Total Price: $result")
+}
